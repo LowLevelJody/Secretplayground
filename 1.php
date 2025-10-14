431788017b3e83a85f755b2620d3ca62e89f108d
Jody Jeremi Hadrian Ritonga <126959866+LowLevelJody@users.noreply.github.com>
2025-10-14 14:15:59 +0700

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,10 +1,10 @@
 <?php
-    header('Content-Description: File Transfer');
-    header('Content-Type: text/plain');
-    header('Content-Disposition: attachment; filename="test"');
+ header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="thisisfromgoogle.html"');
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
     header('Content-Length: 13400');
-    echo "xxxxxxxxxx";
-?>
+    readfile('./evil.html');
+">
